7fd10ca179420343f139daa7bf4f6bbd5c2b09c2
Andrew Stiles <andrewbstiles@gmail.com>
2018-04-02 18:23:52 -0500

    Initial implementation

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var postcss = require('postcss');
+var helpers = require('postcss-message-helpers');
+var color = require('color');
+var reduceFunctionCall = require('reduce-function-call');
 
-module.exports = postcss.plugin('postcss-color-transform', function (opts) {
-  opts = opts || {};
+const COLOR_FUNCTION_REGEX = /\b(rgba?|hsla?)\(/g;
+const COLOR_HEX_REGEX = /#[0-9a-fA-F]{3}(?:[0-9a-fA-F]{3})?\b/g;
 
-  // Work with options here
+module.exports = postcss.plugin('postcss-invert-luminance', function (opts) {
+  let { transform } = opts;
+  if (typeof transform !== 'function') {
+    throw new Error('Must provide a color transform function!');
+  }
 
-  return function (root, result) {
+  function transformColor(c) {
+    return transform(c).rgb().string();
+  }
 
-    // Transform CSS AST here
+  function transformColorFunction(body, functionIdentifier) {
+    return transformColor(color(functionIdentifier + '(' + body + ')'));
+  }
 
+  function transformHex(hexString) {
+    return transformColor(color(hexString));
+  }
+
+  return function (style) {
+    style.walkDecls(function transformDecl(decl) {
+      if (!decl.value) {
+        return;
+      }
+
+      decl.value = helpers.try(function transformColorFunctions() {
+        return reduceFunctionCall(
+          decl.value, COLOR_FUNCTION_REGEX, transformColorFunction);
+      }, decl.source);
+
+      decl.value = helpers.try(function transformHexColors() {
+        return decl.value.replace(COLOR_HEX_REGEX, transformHex);
+      }, decl.source);
+    });
   };
 });
--- index.test.js
+++ index.test.js
@@ -3,17 +3,51 @@ var postcss = require('postcss');
 var plugin = require('./');
 
 function run(input, output, opts) {
-  return postcss([ plugin(opts) ]).process(input)
+  return postcss([ plugin(opts) ]).process(input, { from: undefined })
     .then(result => {
       expect(result.css).toEqual(output);
       expect(result.warnings().length).toBe(0);
     });
 }
 
-/* Write tests here
-
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+it('Transforms hex colors', () => {
+  return run(
+    'a{ color: #FFFF00 }',
+    'a{ color: rgb(0, 0, 255) }',
+    { transform: color => color.negate() });
 });
 
-*/
+it('Transforms short hex colors', () => {
+  return run(
+    'a{ color: #F0F }',
+    'a{ color: rgb(0, 255, 0) }',
+    { transform: color => color.negate() });
+});
+
+it('Transforms rgb colors', () => {
+  return run(
+    'a{ color: rgb(255, 255, 255) }',
+    'a{ color: rgb(0, 0, 0) }',
+    { transform: color => color.negate() });
+});
+
+it('Transforms rgba colors', () => {
+  return run(
+    'a{ color: rgba(255, 255, 255, 0.8) }',
+    'a{ color: rgba(0, 0, 0, 0.8) }',
+    { transform: color => color.negate() });
+});
+
+it('Transforms hsl colors', () => {
+  return run(
+    'a{ color: hsl(90, 50%, 80%) }',
+    'a{ color: rgb(51, 26, 77) }',
+    { transform: color => color.negate() });
+});
+
+it('Transforms hsla colors', () => {
+  return run(
+    'a{ color: hsla(90, 50%, 80%, 0.7) }',
+    'a{ color: rgba(51, 26, 77, 0.7) }',
+    { transform: color => color.negate() });
+});
